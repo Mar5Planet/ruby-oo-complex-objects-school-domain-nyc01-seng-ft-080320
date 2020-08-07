3d37d80dff9b3847a11b336a700c37bdaffb0969
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-07 16:34:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+    if roster[grade]
+      roster[grade] << student
+    else 
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end 
+  
+    def grade(grade_inputted)
+      @roster.filter do |grade, student|
+      grade == grade_inputted
+      end
+    end 
+    
+    def sort
+      @roster.each do |grade, student|
+        student.sort()
+      end
+    end 
+end 
+
+ 
+skool = School.new('My high school')
+binding.pry
